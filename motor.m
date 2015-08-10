f871310b3a8b603370b060411bb41edef187f149
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-10 14:53:29 +1000

    Added motor initialisers.

--- motor.m
+++ motor.m
@@ -1,4 +1,7 @@
 h = COM_OpenNXT(); % Open connection to NXT
 COM_SetDefaultNXT(h); %Set default handle
+mA = NXTMotor('A');
+mB = NXTMotor('B');
+mC = NXTMotor('C');
 % chuck code here
-COM_CloseNXT(COM_GetDefaultNXT());  % close connection to NXT
+%COM_CloseNXT(COM_GetDefaultNXT());  % close connection to NXT
