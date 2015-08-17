88ca8aca65032d47a5866b76af1a43dacf6003c3
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-17 11:23:59 +1000

    Added epsilon value; if the motor tries to move less than a degree, then don't move. If we don't have this, the motor moves forever for small degrees (TachoLimit ~= 0).

--- motor_angles.m
+++ motor_angles.m
@@ -20,23 +20,25 @@ mA_pos = mA_pos + t1;
 mB_pos = mB_pos + t2;
 mC_pos = mC_pos + t3;
 
+epsilon = 1;
+
 % For the first and second joints, the gearing makes the motor behaviour
 % opposite. So we make the motor go forward (gear goes backward) for
 % negative angles and backward (gear goes forward) for positive angles.
 
 % First joint: Positive angles move right, negative angles move left.
-if t1 < 0
+if t1 < -epsilon
     t1 = -t1;
     motor_forward(mA, mA_GR, t1);
-elseif t1 > 0
+elseif t1 > epsilon
     motor_backward(mA, mA_GR, t1);
 end
 
 % Second joint: Positive angles move down, negative angles move up.
-if t2 < 0
+if t2 < -epsilon
     t2 = -t2;
     motor_forward(mB, mB_GR, t2);
-elseif t2 > 0
+elseif t2 > epsilon
     motor_backward(mB, mB_GR, t2);
 end
 
@@ -44,9 +46,9 @@ end
 % result, the behaviour is normal (negative angles move the motor "back",
 % positive angles move the motor "forward"). Positive angles move down,
 % negative angles move up.
-if t3 < 0
+if t3 < -epsilon
     t3 = -t3;
     motor_backward(mC, mC_GR, t3);
-elseif t3 > 0
+elseif t3 > epsilon
     motor_forward(mC, mC_GR, t3);
 end
