91aba6068431177daeb2e479a537f6f97e33fb1e
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-17 11:28:22 +1000

    Adjusted motor function to take in HLU co-ordinates.

--- motor_to.m
+++ motor_to.m
@@ -1,5 +1,6 @@
 function motor_to(x, y, z)
 % Moves the end-effector to x, y, z (general co-ordinates) RELATIVE TO
 % current positioning of the end-effector.
-angles = inv_kine(y, x, z);
+cpos = convert(x, y, z);
+angles = inv_kine(cpos(2), cpos(1), cpos(3));
 motor_angle_set(angles(1), angles(2), angles(3));
