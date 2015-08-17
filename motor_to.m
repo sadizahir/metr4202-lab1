37fcd22d62486b43509f18b20142d646cf9f046e
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-17 10:29:12 +1000

    Swapped x and y arguments for the inverse kinematics solver when directing the motor to a position; don't know why I needed to do that...

--- motor_to.m
+++ motor_to.m
@@ -1,5 +1,5 @@
 function motor_to(x, y, z)
 % Moves the end-effector to x, y, z (general co-ordinates) RELATIVE TO
 % current positioning of the end-effector.
-angles = inv_kine(x, y, z);
+angles = inv_kine(y, x, z);
 motor_angles(angles(1), angles(2), angles(3));
