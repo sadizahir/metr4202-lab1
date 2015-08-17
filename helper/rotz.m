e3d27ef52ee4e6a23fa65264c5eabfcdcb4f7ccd
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-17 10:09:14 +1000

    Suppressed output on rotation helper functions.

--- helper/rotz.m
+++ helper/rotz.m
@@ -1,3 +1,3 @@
 function Rz = rotz(theta)
-    Rz = [cos(theta) -sin(theta) 0 0; sin(theta) cos(theta) 0 0; 0 0 1 0; 0 0 0 1]
+    Rz = [cos(theta) -sin(theta) 0 0; sin(theta) cos(theta) 0 0; 0 0 1 0; 0 0 0 1];
 end
