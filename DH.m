21a588313ac8b5522711df1dffb89da05280c357
Jeff Fisher <doongka@gmail.com>
2015-08-17 12:41:08 +1000

    Updated DH (not to be used)
    Frontend (for running program) created.

--- DH.m
+++ DH.m
@@ -5,22 +5,22 @@ syms theta1 theta2 theta3
 
 T0_d=[1 0 0 0; 0 1 0 0; 0 0 1 d1; 0 0 0 1];
 T0_theta=[cos(theta1) -sin(theta1) 0 0; sin(theta1) cos(theta1) 0 0; 0 0 1 0; 0 0 0 1];
-T0_a =[1 0 0 a1; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-T0_alpha=[1 0 0 0; 0 cos(alpha1) -sin(alpha1) 0;0 sin(alpha1) cos(alpha1) 0; 0 0 0 1];
+T0_a =[1 0 0 0; 0 1 0 a1; 0 0 1 0; 0 0 0 1];
+T0_alpha=[cos(alpha1) 0 sin(alpha1) 0; 0 1 0 0; -sin(alpha1) 0 cos(alpha1) 0; 0 0 0 1];
 
 T0 = T0_d*T0_theta*T0_a*T0_alpha;
 
-T1_d=[1 0 0 0; 0 1 0 0; 0 0 1 d1; 0 0 0 1];
+T1_d=[1 0 0 0; 0 1 0 0; 0 0 1 d2; 0 0 0 1];
 T1_theta=[cos(theta2) -sin(theta2) 0 0; sin(theta2) cos(theta2) 0 0; 0 0 1 0; 0 0 0 1];
-T1_a =[1 0 0 a1; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-T1_alpha=[1 0 0 0; 0 cos(alpha2) -sin(alpha2) 0;0 sin(alpha2) cos(alpha2) 0; 0 0 0 1];
+T1_a =[1 0 0 0; 0 1 0 a2; 0 0 1 0; 0 0 0 1];
+T1_alpha=[cos(alpha2) 0 sin(alpha2) 0; 0 1 0 0; -sin(alpha2) 0 cos(alpha2) 0; 0 0 0 1];
 
 T1 = T1_d*T1_theta*T1_a*T1_alpha;
 
-T2_d=[1 0 0 0; 0 1 0 0; 0 0 1 d1; 0 0 0 1];
+T2_d=[1 0 0 0; 0 1 0 0; 0 0 1 d3; 0 0 0 1];
 T2_theta=[cos(theta3) -sin(theta3) 0 0; sin(theta3) cos(theta3) 0 0; 0 0 1 0; 0 0 0 1];
-T2_a =[1 0 0 a1; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-T2_alpha=[1 0 0 0; 0 cos(alpha3) -sin(alpha3) 0;0 sin(alpha3) cos(alpha3) 0; 0 0 0 1];
+T2_a =[1 0 0 0; 0 1 0 a3; 0 0 1 0; 0 0 0 1];
+T2_alpha=[cos(alpha3) 0 sin(alpha3) 0; 0 1 0 0; -sin(alpha3) 0 cos(alpha3) 0; 0 0 0 1];
 
 T2 = T2_d*T2_theta*T2_a*T2_alpha;
 
