99247e59a2695ffd47a12e4b52edb5cc21c1565f
Kate Read <kate.read@uqconnect.edu.au>
2015-08-17 11:24:07 +1000

    Fixed displacement values

--- convert.m
+++ convert.m
@@ -1,11 +1,11 @@
 %Converts the given path coordinates into mm 
 function coordinates = convert(x, y, z)
-x_d = 70;
-y_d = 160;
-z_d = 155;
+x_d = 80;
+y_d = 175;
+z_d = 180;
 hlutomm_xy = 32;
 hlutomm_z = 20;
-x_new = (x*hlutomm_xy) - x_d;
+x_new = (x*hlutomm_xy) + x_d;
 y_new = (y*hlutomm_xy) - y_d;
 z_new = (z*hlutomm_z) - z_d;
 coordinates = [x_new, y_new, z_new];
