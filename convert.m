ba52d40f53ed4eedbc0b2dda79c7ec4d98c14957
Kate Read <kate.read@uqconnect.edu.au>
2015-08-17 11:15:30 +1000

    Fixed errors

--- convert.m
+++ convert.m
@@ -1,5 +1,5 @@
 %Converts the given path coordinates into mm 
-function coordinates convert(x, y, z)
+function coordinates = convert(x, y, z)
 x_d = 70;
 y_d = 160;
 z_d = 155;
@@ -8,5 +8,5 @@ hlutomm_z = 20;
 x_new = (x*hlutomm_xy) - x_d;
 y_new = (y*hlutomm_xy) - y_d;
 z_new = (z*hlutomm_z) - z_d;
-coordinates(x_new, y_new, z_new);
+coordinates = [x_new, y_new, z_new];
 end
