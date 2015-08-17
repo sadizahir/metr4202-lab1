213aa054b164e5076e4b46872f0410458e28d391
Sadi Zahir <south.wall.cornerclub@gmail.com>
2015-08-17 10:09:39 +1000

    Added smooth start and disabled speed regulation for all motors. Hopefully will make it more accurate?

--- motor.m
+++ motor.m
@@ -5,7 +5,14 @@ global mC;
 h = COM_OpenNXT(); % Open connection to NXT
 COM_SetDefaultNXT(h); %Set default handle
 mA = NXTMotor('A');
+mA.SmoothStart = true;
+mA.SpeedRegulation = false;
 mB = NXTMotor('B');
+mB.SmoothStart = true;
+mB.SpeedRegulation = false;
 mC = NXTMotor('C');
+mC.SmoothStart = true;
+mC.SpeedRegulation = false;
+
 % chuck code here
 %COM_CloseNXT(COM_GetDefaultNXT());  % close connection to NXT
